5022bb1f694ed91c2543c67ade9bd4e4f2c20d0e
Mark Allen <markallen@Pony.local>
2019-12-15 21:09:30 +0000

    Functional tests for multiple items added

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
 from lists.views import home_page
 
@@ -14,6 +13,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
