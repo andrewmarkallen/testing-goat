723b57ccb48fb7d4e9d5d3b77c2b04be368b640a
Mark Allen <markallen@Pony.local>
2020-01-03 17:10:24 +0000

    Moved FTs into their own individual files

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -59,6 +59,7 @@ def _update_static_files(source_folder):
         ' && ../virtualenv/bin/python manage.py collectstatic --noinput'
     )
 
+
 def _update_database(source_folder):
     run(
         f'cd {source_folder}'
